8b3feb504305ed24e454110908dc4e18119cfe00
SyanHsu <101076699+SyanHsu@users.noreply.github.com>
2024-01-25 18:02:03 +0800

    修改结构

---
+++ Assets/Scripts/Base/Singleton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    // 获取单例实例
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject singleton = new GameObject();
+                _instance = singleton.AddComponent<T>();
+                singleton.name = "Singleton_" + typeof(T).ToString();
+
+                DontDestroyOnLoad(singleton);
+            }
+
+            return _instance;
+        }
+    }
+}
+
